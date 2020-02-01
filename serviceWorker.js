690bd6cc5d98d8c832a919f7e48c0e04bc7a1709
MohitBurkule <mohitburkule5@gmail.com>
2020-02-01 21:26:23 +0530

    icon added to service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,7 +5,14 @@ const assets = [
   "/site.html",
   "/index.html",
   "/js/app.js",
-  "/images/icons/icon-144x144.png"
+  "/images/icons/icon-144x144.png",
+  "/images/icons/icon-72x72.png",
+  "/images/icons/icon-96x96.png",
+  "/images/icons/icon-128x128.png",
+  "/images/icons/icon-152x152.png",
+  "/images/icons/icon-192x192.png",
+  "/images/icons/icon-384x384.png",
+  "/images/icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", installEvent => {
