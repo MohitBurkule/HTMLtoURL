a39f46ef65a7a207b80e314c789beecf9a0e6c62
MohitBurkule <mohitburkule5@gmail.com>
2020-02-01 16:47:00 +0530

    pwa

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/site.html",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
+	  console.log("dones");
+    })
+  );
+});
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
