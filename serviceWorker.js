b4b04c2beff7b4f81bc4a73ae53fdab664207684
MohitBurkule <mohitburkule5@gmail.com>
2020-02-02 10:37:56 +0530

    notification click and response

--- serviceWorker.js
+++ serviceWorker.js
@@ -38,6 +38,6 @@ self.addEventListener('notificationclick', function(event) {
   event.notification.close();
 
   event.waitUntil(
-    clients.openWindow('https://developers.google.com/web/')
+    clients.openWindow('http://mysl.gq')
   );
 });
