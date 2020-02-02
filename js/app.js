0e2a5b6479117b1168ef2c0ba6beea5731b0d48a
MohitBurkule <mohitburkule5@gmail.com>
2020-02-02 10:09:28 +0530

    notification to app.js

--- js/app.js
+++ js/app.js
@@ -62,3 +62,21 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
+
+Notification.requestPermission(result => {
+  if (result === 'granted') {
+    showNotification('So nice to have you here!', 'Hey there!')
+  }
+});
+
+function showNotification(title, message) {
+  if ('Notification' in window) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.showNotification(title, {
+        body: message,
+        tag: 'vibration-sample'
+      });
+    });
+  }
+}
+
